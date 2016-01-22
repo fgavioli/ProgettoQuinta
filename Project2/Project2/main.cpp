97e7705c42dd9d63b751fb5f76208c1914bba4a1
GAVIOLI FEDERICO <f.gavioli@VINCI.IISCORNI.IT>
2016-01-22 11:41:32 +0100

    Movimento completato.

--- Project2/Project2/main.cpp
+++ Project2/Project2/main.cpp
@@ -8,6 +8,11 @@
 #include <allegro5\allegro_ttf.h>
 #include <allegro5\allegro_font.h>
 
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
+
 //Strutture utilizzate per ogni personaggio su schermo che descrivono posizionamento e caratteristiche principali personaggi.
 struct recLocation
 {
@@ -49,7 +54,9 @@ void reRender(recPersonaggio test){
 	al_draw_bitmap(test.Sprite, test.Location.X, test.Location.Y, 0);
 	al_flip_display();
 }
+void init(){
 
+}
 //Dichiarazione di variabili "ambientali" necessarie come base per la visualizzazione, quindi globali.
 ALLEGRO_DISPLAY *display = NULL;
 ALLEGRO_DISPLAY_MODE disp_data;
@@ -62,63 +69,95 @@ const float FPS = 60;
 int main(int argc, char **argv){
 	//Dichiarazione variabili personaggi.
 	recPersonaggio test;
-	
+	int key[] = { NULL, NULL, NULL, NULL };
+
 	al_init();
 	al_init_primitives_addon();
 	al_init_image_addon();
 	al_install_keyboard();
 	timer = al_create_timer(1.0 / FPS);
-
-	test = initrecPersonaggio(10, 10, al_load_bitmap("1.PNG"), initrecLocation(10, 10), true);
-
 	//al_set_new_display_flags(ALLEGRO_FULLSCREEN);
 	display = al_create_display(1366, 768);
-
 	event_queue = al_create_event_queue();
 	al_register_event_source(event_queue, al_get_display_event_source(display));
 	al_register_event_source(event_queue, al_get_timer_event_source(timer));
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
+
+	test = initrecPersonaggio(10, 10, al_load_bitmap("1.PNG"), initrecLocation(10, 10), true);
 	al_clear_to_color(al_map_rgb(0, 0, 0));
 	al_draw_bitmap(test.Sprite, test.Location.X, test.Location.Y, NULL);
 	al_flip_display();
-	
-	//al_start_timer(timer);
+	al_start_timer(timer);
+	bool redraw = false;
 	while (1)
 	{
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(event_queue, &ev);
-		
-		if (ev.type == ALLEGRO_EVENT_KEY_DOWN)
-		{
-			switch (ev.keyboard.keycode)
-			{
-			case ALLEGRO_KEY_W:
-				test.Location.Y -= test.MoveSpeed;
-				reRender(test);
-				break;
 
-			case ALLEGRO_KEY_S:
+		if (ev.type == ALLEGRO_EVENT_TIMER) {
+			if (key[UP]) {
+				test.Location.Y -= test.MoveSpeed;
+			}
+
+			if (key[DOWN]) {
 				test.Location.Y += test.MoveSpeed;
-				reRender(test);
+			}
+
+			if (key[LEFT]) {
+				test.Location.X -= test.MoveSpeed;
+			}
+
+			if (key[RIGHT]) {
+				test.Location.X += test.MoveSpeed;
+			}
+
+			redraw = true;
+		}
+		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			break;
+		}
+		else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+			switch (ev.keyboard.keycode) {
+			case ALLEGRO_KEY_W:
+				key[UP] = true;
+				break;
+				 
+			case ALLEGRO_KEY_S:
+				key[DOWN] = true;
 				break;
 
 			case ALLEGRO_KEY_A:
-				test.Location.X -= test.MoveSpeed;
-				reRender(test);
+				key[LEFT] = true;
 				break;
 
 			case ALLEGRO_KEY_D:
-				test.Location.X += test.MoveSpeed;
-				reRender(test);
+				key[RIGHT] = true;
+				break;
+			}
+		}
+		else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
+			switch (ev.keyboard.keycode) {
+			case ALLEGRO_KEY_W:
+				key[UP] = false;
+				break;
+
+			case ALLEGRO_KEY_S:
+				key[DOWN] = false;
+				break;
+
+			case ALLEGRO_KEY_A:
+				key[LEFT] = false;
+				break;
+
+			case ALLEGRO_KEY_D:
+				key[RIGHT] = false;
 				break;
 			}
 		}
 
-		if (ev.type == ALLEGRO_EVENT_TIMER) {
+		if (redraw && al_is_event_queue_empty(event_queue)) {
+			redraw = false;
 			reRender(test);
-		}
-		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-			break;
 		}
 	}
 
