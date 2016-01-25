96c328ca7a45c7b0dd24f65cba0e78ce88ace053
GAVIOLI FEDERICO <f.gavioli@VINCI.IISCORNI.IT>
2016-01-25 10:59:38 +0100

    Strutturazione progetto con commenti

--- Project2/Project2/main.cpp
+++ Project2/Project2/main.cpp
@@ -12,6 +12,7 @@
 #define DOWN 1
 #define LEFT 2
 #define RIGHT 3
+#define MAX_CHARACTERS 50
 
 //Strutture utilizzate per ogni personaggio su schermo che descrivono posizionamento e caratteristiche principali personaggi.
 struct recLocation
@@ -21,14 +22,14 @@ struct recLocation
 };
 struct recPersonaggio
 {
+	char *name;
 	short MoveSpeed;
 	short HealthPoints;
-	short MovAnime = 0;
 	ALLEGRO_BITMAP *Sprite;
 	recLocation Location;
 	bool Visible = false;
 };
-//funzione di inizializzazione per maggiore pulizia del codice.
+//funzioni di inizializzazione per maggiore pulizia del codice.
 recLocation initrecLocation(int X, int Y){
 	recLocation r;
 	r.X = X;
@@ -44,84 +45,83 @@ recPersonaggio initrecPersonaggio(short MoveSpeed, short HealthPoints, ALLEGRO_B
 	r.Visible = isVisible;
 	return r;
 }
-//funzione di movimento utilizzata per ogni personaggio su schermo: dato il personaggio da muovere e la posizione obiettivo vengono settate la x e la y dell'oggetto e richiamata la funzione reRender.
+//funzione di movimento utilizzata per ogni personaggio su schermo.
 void move(recPersonaggio &target, int targetX, int targetY){
 	target.Location.X = targetX;
 	target.Location.Y = targetY;
 }
-//Funzione che cancella ogni oggetto su schermo e lo ridisegna se il parametro visible dell'oggetto stesso è true.
-void reRender(recPersonaggio test){
+
+//Procedura che cancella ogni oggetto su schermo e lo ridisegna se il parametro visible dell'oggetto stesso è true.
+void reRender(){
 	al_clear_to_color(al_map_rgb(0, 0, 0));
-	al_draw_bitmap(test.Sprite, test.Location.X, test.Location.Y, 0);
+	for (short i = 0; i < npg; i++)
+		if (personaggi[i].Visible)
+			al_draw_bitmap(personaggi[i].Sprite, personaggi[i].Location.X, personaggi[i].Location.Y, 0);
 	al_flip_display();
 }
-void reAnime(recPersonaggio test){
-	if (test.MovAnime == 0){
-		test = initrecPersonaggio(10, 10, al_load_bitmap("1.PNG"), initrecLocation(10, 10), true);
-		test.MovAnime++;
-	}
-	else
-	{
-		test = initrecPersonaggio(10, 10, al_load_bitmap("2.JPG"), initrecLocation(10, 10), true);
-		test.MovAnime--;
-	}
 
-}
-void init(){
-
-}
-//Dichiarazione di variabili "ambientali" necessarie come base per la visualizzazione, quindi globali.
-ALLEGRO_DISPLAY *display = NULL;
-ALLEGRO_DISPLAY_MODE disp_data;
-ALLEGRO_BITMAP *sfondo = NULL;
-ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-ALLEGRO_TIMER *timer = NULL;
-
-const float FPS = 60;
-
-int main(int argc, char **argv){
-	//Dichiarazione variabili personaggi.
-	recPersonaggio test;
-	int key[] = { NULL, NULL, NULL, NULL };
-
+//Procedure per inizializzare variabili di evento e addons allegro.
+void initAddons(){
 	al_init();
 	al_init_primitives_addon();
 	al_init_image_addon();
 	al_install_keyboard();
-	timer = al_create_timer(1.0 / FPS);
-	//al_set_new_display_flags(ALLEGRO_FULLSCREEN);
-	display = al_create_display(1366, 768);
+}
+void initEvents(ALLEGRO_EVENT_QUEUE *event_queue){
 	event_queue = al_create_event_queue();
-	al_register_event_source(event_queue, al_get_display_event_source(display));
-	al_register_event_source(event_queue, al_get_timer_event_source(timer));
-	al_register_event_source(event_queue, al_get_keyboard_event_source());
+	al_register_event_source(coda, al_get_display_event_source(display));
+	al_register_event_source(coda, al_get_timer_event_source(timer));
+	al_register_event_source(coda, al_get_keyboard_event_source());
+}
 
-	test = initrecPersonaggio(10, 10, al_load_bitmap("1.PNG"), initrecLocation(10, 10), true);
+//Dichiarazione di variabili "ambientali" necessarie come base per la visualizzazione, quindi globali.
+ALLEGRO_DISPLAY *display = NULL;
+ALLEGRO_BITMAP *sfondo = NULL;
+ALLEGRO_EVENT_QUEUE *coda = NULL;
+ALLEGRO_TIMER *timer = NULL;
+int buffer[4] = { NULL, NULL, NULL, NULL };
+
+recPersonaggio personaggi[MAX_CHARACTERS];
+int npg;
+
+const float FPS = 60;
+
+int main(int argc, char **argv){
+
+	initAddons();
+	display = al_create_display(1366, 768);
+	timer = al_create_timer(1.0 / FPS);
+
+	//al_set_new_display_flags(ALLEGRO_FULLSCREEN);
+	initEvents(coda);
+
+	personaggi[0] = initrecPersonaggio(10, 10, al_load_bitmap("1.PNG"), initrecLocation(10, 10), true);
+	
 	al_clear_to_color(al_map_rgb(0, 0, 0));
-	al_draw_bitmap(test.Sprite, test.Location.X, test.Location.Y, NULL);
+	al_draw_bitmap(personaggi[0].Sprite, personaggi[0].Location.X, personaggi[0].Location.Y, NULL);
 	al_flip_display();
 	al_start_timer(timer);
 	bool redraw = false;
 	while (1)
 	{
 		ALLEGRO_EVENT ev;
-		al_wait_for_event(event_queue, &ev);
+		al_wait_for_event(coda, &ev);
 
 		if (ev.type == ALLEGRO_EVENT_TIMER) {
-			if (key[UP]) {
-				test.Location.Y -= test.MoveSpeed;
+			if (buffer[UP]) {
+				personaggi[0].Location.Y -= personaggi[0].MoveSpeed;
 			}
 
-			if (key[DOWN]) {
-				test.Location.Y += test.MoveSpeed;
+			if (buffer[DOWN]) {
+				personaggi[0].Location.Y += personaggi[0].MoveSpeed;
 			}
 
-			if (key[LEFT]) {
-				test.Location.X -= test.MoveSpeed;
+			if (buffer[LEFT]) {
+				personaggi[0].Location.X -= personaggi[0].MoveSpeed;
 			}
 
-			if (key[RIGHT]) {
-				test.Location.X += test.MoveSpeed;
+			if (buffer[RIGHT]) {
+				personaggi[0].Location.X += personaggi[0].MoveSpeed;
 			}
 
 			redraw = true;
@@ -132,59 +132,51 @@ int main(int argc, char **argv){
 		else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
 			switch (ev.keyboard.keycode) {
 			case ALLEGRO_KEY_W:
-				key[UP] = true;
-				reAnime(test);
+				buffer[UP] = true;
 				break;
 				 
 			case ALLEGRO_KEY_S:
-				key[DOWN] = true;
-				reAnime(test);
+				buffer[DOWN] = true;
 				break;
 
 			case ALLEGRO_KEY_A:
-				key[LEFT] = true;
-				reAnime(test);
+				buffer[LEFT] = true;
 				break;
 
 			case ALLEGRO_KEY_D:
-				key[RIGHT] = true;
-				reAnime(test);
+				buffer[RIGHT] = true;
 				break;
 			}
 		}
 		else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
 			switch (ev.keyboard.keycode) {
 			case ALLEGRO_KEY_W:
-				key[UP] = false;
-				reAnime(test);
+				buffer[UP] = false;
 				break;
 
 			case ALLEGRO_KEY_S:
-				key[DOWN] = false;
-				reAnime(test);
+				buffer[DOWN] = false;
 				break;
 
 			case ALLEGRO_KEY_A:
-				key[LEFT] = false;
-				reAnime(test);
+				buffer[LEFT] = false;
 				break;
 
 			case ALLEGRO_KEY_D:
-				key[RIGHT] = false;
-				reAnime(test);
+				buffer[RIGHT] = false;
 				break;
 			}
 		}
 
-		if (redraw && al_is_event_queue_empty(event_queue)) {
+		if (redraw && al_is_event_queue_empty(coda)) {
 			redraw = false;
-			reRender(test);
+			reRender();
 		}
 	}
 
 	al_destroy_timer(timer);
 	al_destroy_display(display);
-	al_destroy_event_queue(event_queue);
+	al_destroy_event_queue(coda);
 	
 	return 0;
 }
