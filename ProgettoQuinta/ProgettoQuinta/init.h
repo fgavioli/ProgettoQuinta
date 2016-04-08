1a3b1f381dfd4c633b7f8ee35a5f274c5cde7510
Akwry <masteradrix@gmail.com>
2016-04-08 11:13:26 +0200

    aggiunta cartella immagini con immagine del bg

--- ProgettoQuinta/ProgettoQuinta/init.h
+++
@@ -1,9 +0,0 @@
-#include <allegro5\allegro.h>
-#include <allegro5\allegro_image.h>
-#include <allegro5\allegro_audio.h>
-#include <allegro5\allegro_acodec.h>
-
-void initBase();
-void initImg();
-void initAudio();
-void initKeyboard();
--- ProgettoQuinta/ProgettoQuinta/schermo.h
+++
@@ -1,9 +0,0 @@
-#include <allegro5\allegro.h>
-
-struct recSchermata {
-	ALLEGRO_DISPLAY *schermo = NULL;
-	ALLEGRO_DISPLAY_MODE disp_data1;
-};
-
-recSchermata initrecSchermata(recSchermata test, int schermoX, int schermoY);
-void sudoku_schermo(recSchermata test);
